c8287f95a14ce92194ee71f4cfd5403486d66d26
Anupam <anupam.iiita@gmail.com>
2018-08-17 13:29:33 +0200

    Carthage Support

---
+++ PodInstallTarget/LightStep/LightStep.h
@@ -0,0 +1,19 @@
+//
+//  LightStep.h
+//  LightStep
+//
+//  Created by Anupam Choudhary on 17/08/18.
+//  Copyright © 2018 LightStep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LightStep.
+FOUNDATION_EXPORT double LightStepVersionNumber;
+
+//! Project version string for LightStep.
+FOUNDATION_EXPORT const unsigned char LightStepVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LightStep/PublicHeader.h>
+
+
